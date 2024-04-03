5cf75c48234fc4365aa3df00b5600ef10a164387
manojmarquez33 <manoj17kumar10@gmail.com>
2024-04-03 08:05:41 +0530

    title changed

--- src/components/dashboard/shared/navbar/Navbar.jsx
+++ src/components/dashboard/shared/navbar/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div className="navbar">
       <div className="logo">
         <img src="logo.svg" alt="" />
-        <span>Karthikeyani</span>
+        <span>DOP Tech Blazers</span>
       </div>
       <div className="icons">
         <img src="/search.svg" alt="" className="icon" />
