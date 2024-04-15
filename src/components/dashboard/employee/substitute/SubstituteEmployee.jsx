b4e07df430d2a25117b93c69b7e55a603d3476e5
Mohankumar R <114681544+Viyaga2022@users.noreply.github.com>
2024-04-15 07:27:45 +0530

    Update SubstituteEmployee.jsx

--- src/components/dashboard/employee/substitute/SubstituteEmployee.jsx
+++ src/components/dashboard/employee/substitute/SubstituteEmployee.jsx
@@ -9,7 +9,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { setSubstituteEmployee } from "@/redux/slices/commonSlice";
 import DataTableWithActions from "../../shared/dataTableWithActions/DataTableWithActions";
 import AddRegularEmployee from "./addSubstituteEmployee/AddSubstituteEmployee";
-import DeleteSubstituteEmployee from "./deleteSubstituteEMployee/DeleteSubstituteEMployee";
+import DeleteSubstituteEmployee from "./deleteSubstituteEmployee/DeleteSubstituteEmployee";
 
 const SubstituteEmployee = () => {
     const { substitute } = useSelector(state => state.common.employee)
